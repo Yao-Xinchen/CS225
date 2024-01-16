21061fb26c3f05280324454ae4f8eec7ded26221
Yao-Xinchen <yao29@illinois.edu>
2024-01-16 14:36:20 +0800

    readme added, build removed

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 225 in CMake
+
+CS 225 ZJUI SP2024
+
+Written by Yao Xinchen, Jun 2024
--- cmake-build-debug/lab_intro/CMakeFiles/lab_intro.dir/cs225/HSLAPixel.cpp.o.d
+++
@@ -1,4 +0,0 @@
-lab_intro/CMakeFiles/lab_intro.dir/cs225/HSLAPixel.cpp.o: \
- /home/xinchen/Dev/CS225_test/lab_intro/cs225/HSLAPixel.cpp \
- /usr/include/stdc-predef.h \
- /home/xinchen/Dev/CS225_test/lab_intro/cs225/HSLAPixel.h
