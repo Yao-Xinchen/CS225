e8dc7c4d481bdd7c408513818369d64924b232ea
Yao-Xinchen <yao29@illinois.edu>
2024-01-22 10:17:35 +0800

    lab_debug completed

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -6,46 +6,51 @@
 using namespace cs225;
 
 // sets up the output image
-PNG* setupOutput(unsigned w, unsigned h) {
-    PNG* image = new PNG(w, h);
+PNG* setupOutput(const unsigned w, const unsigned h)
+{
+    const auto image = new PNG(w, h);
     return image;
 }
 
 // Returns my favorite color
-HSLAPixel* myFavoriteColor(double saturation) {
-    HSLAPixel pixel(-1, saturation, 0.5);
-    return &pixel;
+HSLAPixel* myFavoriteColor(double saturation)
+{
+    const auto pixel = new HSLAPixel(280, saturation, 0.5); // hue can be changed
+    return pixel;
 }
 
-void sketchify(std::string inputFile, std::string outputFile) {
+void sketchify(std::string inputFile, std::string outputFile)
+{
     // Load in.png
-    PNG* original = NULL;
+    const auto original = new PNG();
 
     original->readFromFile(inputFile);
-    unsigned width = original->width();
-    unsigned height = original->height();
+    const unsigned width = original->width();
+    const unsigned height = original->height();
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(0.5);
+    const auto myPixel = myFavoriteColor(0.75); // saturation can be changed
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++)
+    {
+        for (unsigned x = 1; x < width; x++)
+        {
             // Calculate the pixel difference
-            HSLAPixel* prev = original->getPixel(x - 1, y - 1);
-            HSLAPixel* curr = original->getPixel(x, y);
-            double diff = std::fabs(curr->h - prev->h);
+            const auto prev = original->getPixel(x - 1, y - 1);
+            const auto curr = original->getPixel(x, y);
+            const double diff = std::fabs(curr->h - prev->h);
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel* currOutPixel = (*output).getPixel(x, y);
-            if (diff > 20) {
-                currOutPixel = myPixel;
+            HSLAPixel* currOutPixel = output->getPixel(x, y);
+            if (diff > 20)
+            {
+                *currOutPixel = *myPixel;
             }
         }
     }
