ea8a7e622d2ceaf4a82f38f37e2b1f5aca81f69e
Yao-Xinchen <yao29@illinois.edu>
2024-01-22 09:37:05 +0800

    mp1 completed

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -1,6 +1,24 @@
+#include "cs225/PNG.h"
 #include <string>
 
 void rotate(std::string inputFile, std::string outputFile)
 {
+    auto input_image = cs225::PNG();
+    input_image.readFromFile(inputFile);
+    auto output_image = cs225::PNG(input_image.width(), input_image.height());
 
+    const auto width = static_cast<int>(input_image.width());
+    const auto height = static_cast<int>(input_image.height());
+
+    for (auto x = 0; x < width; x++)
+    {
+        for (auto y = 0; y < height; y++)
+        {
+            const auto input = input_image.getPixel(x, y);
+            const auto output = output_image.getPixel(width - x - 1, height - y - 1);
+            *output = *input;
+        }
+    }
+
+    output_image.writeToFile(outputFile);
 }
