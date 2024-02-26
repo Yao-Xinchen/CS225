1f816ba390610c971fa3d077ccce01a781c8a6b6
Yao-Xinchen <yao29@illinois.edu>
2024-02-26 09:39:01 +0800

    mp2 created, remove all executables

---
+++ mp2/StickerSheet.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Yao Xinchen on 2024/2/26.
+//
+
+#include "StickerSheet.h"
+
+namespace cs225
+{
+} // cs225
---
+++ mp2/StickerSheet.h
@@ -0,0 +1,18 @@
+//
+// Created by Yao Xinchen on 2024/2/26.
+//
+
+#ifndef CS225_STICKERSHEET_H
+#define CS225_STICKERSHEET_H
+
+namespace cs225
+{
+
+    class StickerSheet
+    {
+
+    };
+
+} // cs225
+
+#endif //CS225_STICKERSHEET_H
