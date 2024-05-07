8dc27ddfa651d4db2e1d9bba699ce59fcbef6cfe
Yao-Xinchen <yao29@illinois.edu>
2024-05-07 15:32:44 +0800

    mp7 structure completed

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,23 @@
 /* Your code here! */
+
+#include "dsets.h"
+
+void DisjointSets::addelements(int num)
+{
+    return;
+}
+
+int DisjointSets::find(int elem)
+{
+    return 0;
+}
+
+void DisjointSets::setunion(int a, int b)
+{
+    return;
+}
+
+int DisjointSets::size(int elem)
+{
+    return 0;
+}
--- mp7/maze.cpp
+++ mp7/maze.cpp
@@ -1,1 +1,38 @@
 /* Your code here! */
+
+#include "maze.h"
+
+SquareMaze::SquareMaze()
+{
+    return;
+}
+
+void SquareMaze::makeMaze(int width, int height)
+{
+    return;
+}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const
+{
+    return false;
+}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists)
+{
+    return;
+}
+
+vector<int> SquareMaze::solveMaze()
+{
+    return {};
+}
+
+PNG* SquareMaze::drawMaze() const
+{
+    return {};
+}
+
+PNG* SquareMaze::drawMazeWithSolution()
+{
+    return {};
+}
